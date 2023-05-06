1b00c1ece9dc7b04ed4d34785a6d0c6bbad52096
dlizotte-uwo <dlizotte@uwo.ca>
2023-05-06 13:25:05 +0000

    Re-factored and added smartplug class

--- aioeagle/salus_smartplug.py
+++ aioeagle/salus_smartplug.py
@@ -1,30 +1,29 @@
 from .util import xmltodict_ensure_list, create_command
 from .zigbee_device import ZigbeeDevice
 
-class ElectricMeter(ZigbeeDevice):
-    """Represent an electric meter.
+class SX885ZB(ZigbeeDevice):
+    """Represent a smart plug.
 
     {'ConnectionStatus': 'Connected',
       'HardwareAddress': '0x00abcd123',
       'LastContact': '0x611d885a',
       'Manufacturer': 'Generic',
-      'ModelId': 'electric_meter',
-      'Name': 'Power Meter',
-      'NetworkAddress': '0x0000',
+      'ModelId': 'SX885ZB',
+      'Name': 'My Plug',
+      'NetworkAddress': '0xABCD',
       'Protocol': 'Zigbee'}
     """
 
-    # Bug in the API is causing CurrentSummationDelivered not
-    # to be returned. So just fetch all (default behavior)
     ENERGY_AND_POWER_VARIABLES = [
         "zigbee:InstantaneousDemand",
         "zigbee:CurrentSummationDelivered",
-        "zigbee:CurrentSummationReceived",
     ]
+
+    CONTROL_VARIABLE = "zigbee:OnOff"
 
     @classmethod
     def create_instance(cls, hub, hardware_address):
-        """Create a new electric meter."""
+        """Create a new smart plug."""
         return cls(
             {
                 "HardwareAddress": hardware_address,
@@ -33,4 +32,4 @@ class ElectricMeter(ZigbeeDevice):
         )
 
     def __repr__(self) -> str:
-        return f"<ElectricMeter {self.details.get('Name', '')}>"
+        return f"<SX885ZB {self.details.get('Name', '')}>"
